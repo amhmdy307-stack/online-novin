52268cedc48d36815184b15ed40192a551a4353d
amhmdy307-stack <amhmdy307@gmail.com>
2026-08-16 02:04:43 +0330

    Update app.py

--- app.py
+++ app.py
@@ -247,5 +247,4 @@ def health(): return jsonify(ok=True)
 if __name__=="__main__":
     init_db()
     app.run(host="0.0.0.0", port=int(os.environ.get("PORT",5000)))
-from database import create_tables
-create_tables()
+
